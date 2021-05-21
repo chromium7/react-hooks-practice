f8a61e669cbdd4b8bce80798a1950e777ff17ea5
Christofer Saputra <christofersaputra29@gmail.com>
2021-05-21 21:15:30 +0700

    Update bookings grid component

--- src/components/Bookings/BookingsGrid.js
+++ src/components/Bookings/BookingsGrid.js
@@ -32,11 +32,52 @@ export default function BookingsGrid({ week, bookable, booking, setBooking }) {
     }
   }, [week, bookable, setBookings]);
 
+  function cell(session, date) {
+    const cellData = bookings?.[session]?.[date] || grid[session][date];
+    const isSelected = booking?.session === session && booking?.date === date;
+    return (
+      <td
+        key={date}
+        className={isSelected ? "selected" : null}
+        onClick={bookings ? () => setBooking(cellData) : null}
+      >
+        {cellData.title}
+      </td>
+    );
+  }
+
+  if (!grid) {
+    return <p>Loading ....</p>;
+  }
+
   return (
-    <div className="bookings-grid placeholder">
-      <h3>Bookings Grid</h3>
-      <p>{bookable?.title}</p>
-      <p>{week.date.toISOString()}</p>
-    </div>
+    <Fragment>
+      {error && (
+        <p className="bookingsError">{`There was a problem loading the bookings data(${error})`}</p>
+      )}
+
+      <table className={bookings ? "bookingsGrid active" : "bookingsGrid"}>
+        <thead>
+          <tr>
+            <th>
+              <span className="status">
+                <Spinner />
+              </span>
+            </th>
+            {dates.map((d) => (
+              <th key={d}>{new Date(d).toDateString()}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {sessions.map((session) => (
+            <tr key={session}>
+              <th>{session}</th>
+              {dates.map((date) => cell(session, date))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Fragment>
   );
 }
