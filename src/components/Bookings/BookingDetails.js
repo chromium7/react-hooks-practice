3ec862bc619857a6386e4b06e7739a43d1c12ddf
Christofer Saputra <christofersaputra29@gmail.com>
2021-05-22 09:10:52 +0700

    Update css and db.json

--- src/components/Bookings/BookingDetails.js
+++ src/components/Bookings/BookingDetails.js
@@ -1,7 +1,16 @@
-export default function BookingDetails() {
+import Booking from "./Booking";
+
+export default function BookingDetails({ booking, bookable }) {
   return (
-    <div className="booking-details placeholder">
-      <h3>Booking details</h3>
+    <div className="booking-details">
+      <h2>Booking details</h2>
+      {booking ? (
+        <Booking booking={booking} bookable={bookable} />
+      ) : (
+        <div className="booking-details-fields">
+          <p>Select a booking or a booking slot.</p>
+        </div>
+      )}
     </div>
   );
 }
