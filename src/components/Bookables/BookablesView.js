a26b006d24fead9a01e218fd8755fa98317f3f6e
Christofer Saputra <christofersaputra29@gmail.com>
2021-05-24 22:20:20 +0700

    Update bookable page with useParams and useNavigate hooks

--- src/components/Bookables/BookablesView.js
+++ src/components/Bookables/BookablesView.js
@@ -28,24 +28,43 @@
 //   );
 // }
 
-import { useState, useCallback, Fragment } from "react";
+import { Link, useParams } from "react-router-dom";
+import { FaPlus } from "react-icons/fa";
 
 import BookablesList from "./BookablesList";
 import BookableDetails from "./BookableDetails";
+import PageSpinner from "../UI/PageSpinner";
+
+import useFetch from "../../utils/useFetch";
 
 export default function BookablesView() {
-  const [bookable, setBookable] = useState();
+  const { data: bookables = [], status, error } = useFetch("http://localhost:3001/bookables");
 
-  const updateBookable = useCallback((selected) => {
-    if (selected) {
-      selected.lastShown = Date.now();
-      setBookable(selected);
-    }
-  }, []);
+  const { id } = useParams();
+  const bookable = bookables.find((b) => b.id === parseInt(id, 10)) || bookables[0];
+
+  if (status === "error") {
+    return <p>{error.message}</p>;
+  }
+  if (status === "loading") {
+    return <PageSpinner />;
+  }
   return (
-    <Fragment>
-      <BookablesList bookable={bookable} setBookable={updateBookable} />
+    <main className="bookables-page">
+      <div>
+        <BookablesList
+          bookable={bookable}
+          bookables={bookables}
+          getUrl={(id) => `/bookables/${id}`}
+        />
+        <p className="controls">
+          <Link to="/bookables/new" replace={true} className="btn">
+            <FaPlus />
+            <span>New</span>
+          </Link>
+        </p>
+      </div>
       <BookableDetails bookable={bookable} />
-    </Fragment>
+    </main>
   );
 }
