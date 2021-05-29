e2338abcea99bdfc4942d7c2bf334b5b6838f257
Christofer Saputra <christofersaputra29@gmail.com>
2021-05-29 20:08:56 +0700

    Add fallback avatar for user

--- src/components/Users/UserDetails.js
+++ src/components/Users/UserDetails.js
@@ -1,5 +1,6 @@
 import { useQuery } from "react-query";
 import getData from "../../utils/api";
+import Avatar from "./Avatar";
 
 export default function UserDetails({ userId }) {
   const { data: user } = useQuery(
@@ -14,9 +15,11 @@ export default function UserDetails({ userId }) {
       <div className="item-header">
         <h2>{user.name}</h2>
       </div>
-      <div className="user-avatar">
-        <img src={`http://localhost:3001/img/${user.img}`} alt={user.name} />
-      </div>
+      <Avatar
+        src={`http://localhost:3001/img/${user.img}`}
+        fallbackSrc={"http://localhost:3001/img/avatar.gif"}
+        alt={user.name}
+      />
       <div className="user-details">
         <h3>{user.title}</h3>
         <p>{user.notes}</p>
